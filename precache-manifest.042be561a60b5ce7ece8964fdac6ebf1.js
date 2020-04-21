20afb6944663d75afad1705478efb24f7396a538
sparethme <spare@ualberta.ca>
2020-04-21 10:33:16 -0400

    Updates

---
+++ precache-manifest.042be561a60b5ce7ece8964fdac6ebf1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f46bd13312640125fec70f24aef7d82",
+    "url": "/insure-lp-fem/index.html"
+  },
+  {
+    "revision": "8d51565e981bf842c9b0",
+    "url": "/insure-lp-fem/static/css/main.c6e33689.chunk.css"
+  },
+  {
+    "revision": "b210090fe4ce0078a7b4",
+    "url": "/insure-lp-fem/static/js/2.d9e05a9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insure-lp-fem/static/js/2.d9e05a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d51565e981bf842c9b0",
+    "url": "/insure-lp-fem/static/js/main.76a76f28.chunk.js"
+  },
+  {
+    "revision": "be061fc931c42726fb55",
+    "url": "/insure-lp-fem/static/js/runtime-main.ba33da26.js"
+  },
+  {
+    "revision": "c33e3a526c0d772e3611831d1e3fc838",
+    "url": "/insure-lp-fem/static/media/bg-pattern-footer-mobile.c33e3a52.svg"
+  },
+  {
+    "revision": "a4de14b0ee74ee9f31076b875f4ef78e",
+    "url": "/insure-lp-fem/static/media/bg-pattern-how-we-work-mobile.a4de14b0.svg"
+  },
+  {
+    "revision": "562149a81a4a5d64fcb7309999bdbcf9",
+    "url": "/insure-lp-fem/static/media/bg-pattern-intro-left-mobile.562149a8.svg"
+  },
+  {
+    "revision": "ef3d23bd373b877c0f109cf00fccdd0d",
+    "url": "/insure-lp-fem/static/media/bg-pattern-intro-right-mobile.ef3d23bd.svg"
+  },
+  {
+    "revision": "b9dc0b5c6bd0cf4ddcd5ce9f3ad16897",
+    "url": "/insure-lp-fem/static/media/icon-affordable-prices.b9dc0b5c.svg"
+  },
+  {
+    "revision": "d57cc9d5ede81487205ac0071030bb57",
+    "url": "/insure-lp-fem/static/media/icon-close.d57cc9d5.svg"
+  },
+  {
+    "revision": "6fd0e32479e7d8af8e471d2a34244e93",
+    "url": "/insure-lp-fem/static/media/icon-facebook.6fd0e324.svg"
+  },
+  {
+    "revision": "d98c1990131df21b2dc25cd8c1382819",
+    "url": "/insure-lp-fem/static/media/icon-hamburger.d98c1990.svg"
+  },
+  {
+    "revision": "48a204c8175de66e9c8f1b9207cc77eb",
+    "url": "/insure-lp-fem/static/media/icon-instagram.48a204c8.svg"
+  },
+  {
+    "revision": "b264095b9d9862f3cc93d449508b65fd",
+    "url": "/insure-lp-fem/static/media/icon-people-first.b264095b.svg"
+  },
+  {
+    "revision": "275a65a2fef7bff622ba4fb6e7aa57eb",
+    "url": "/insure-lp-fem/static/media/icon-pinterest.275a65a2.svg"
+  },
+  {
+    "revision": "3cc5dc39b6297427126b6a7389e9b76d",
+    "url": "/insure-lp-fem/static/media/icon-snappy-process.3cc5dc39.svg"
+  },
+  {
+    "revision": "6eef62ff8487d0c9808c68e74d68d9c8",
+    "url": "/insure-lp-fem/static/media/icon-twitter.6eef62ff.svg"
+  },
+  {
+    "revision": "8ac69ec7c30fa6465fa05426a4b235f4",
+    "url": "/insure-lp-fem/static/media/image-intro-desktop.8ac69ec7.jpg"
+  },
+  {
+    "revision": "09a91c1a381a6fc1ecb8512490b74a12",
+    "url": "/insure-lp-fem/static/media/image-intro-mobile.09a91c1a.jpg"
+  },
+  {
+    "revision": "1bc452ac6f5856ef32a25e4519a4db91",
+    "url": "/insure-lp-fem/static/media/logo.1bc452ac.svg"
+  }
+]);
